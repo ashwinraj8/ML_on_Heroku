e9316e8347bb6dbf88fdb559694cfcd9d6365fc0
azz-design <53371325+azz-design@users.noreply.github.com>
2020-02-22 22:49:47 +0530

    ml_on_heroku

---
+++ model.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 22 19:03:13 2020
+
+@author: user
+"""
+
+import numpy as np
+import pandas as pd
+import pickle
+
+dataset  = pd.read_csv('hiring.csv')
+
+dataset['experience'].fillna(0, inplace=True)
+
+dataset['test_score'].fillna(dataset['test_score'].mean(),inplace=True)
+
+X =  dataset.iloc[:,:3]
+
+#converting word to integer
+
+def convert_to_int(word):
+    word_dict = {'one':1, 'two':2,'three':3,'four':4, 'five':5,'six':6,'seven':7,'eight':8,'nine':9,'ten':10,
+                 'eleven':11,'twelve':12,'zero':0 ,0:0}
+    return word_dict[word]
+
+X['experience'] = X['experience'].apply(lambda x: convert_to_int(x))
+
+y = dataset.iloc[:,-1]
+
+#Splitting trainin test set
+
+#less data training on available data 
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+
+#Fitting model to training data
+regressor.fit(X,y)
+
+pickle.dump(regressor,open('model.pkl','wb'))
+
+#loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[2,4,6]]))
+
